75de43af614da0fb7ef1da783974d32a61e93e29
Ali <72079772+alir1986@users.noreply.github.com>
2025-05-20 16:47:48 +0400

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Extra Note on Sales Order', 
+    'author' : 'Concept Solutio LLC',
+    'sequence' : '0',
+    'website' : 'www.conceptgrps.com',
+    'summary' : 'odoo 17 Developtment' ,
+    'installable': True,
+    'application': True,
+    'version': '1.0',
+    'category': 'administrator',
+    'depends' : [ 'sale_management'],
+    'data' : [
+        'views/sales_order_view.xml',
+    ],
+}
